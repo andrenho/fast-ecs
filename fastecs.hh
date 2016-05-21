d73400fc7f796872a5a6fa5af4455c2b181e870f
André Wagner <andre.nho@gmail.com>
2016-05-21 16:30:49 -0300

    .

---
+++ fastecs.hh
@@ -0,0 +1,151 @@
+#ifndef FASTECS_HH_
+#define FASTECS_HH_
+
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <limits>
+#include <vector>
+
+#include <gtest/gtest_prod.h>
+
+namespace ECS {
+
+class Engine {
+private:
+    template<typename entity_size_t    = int32_t,
+             typename component_id_t   = uint16_t,
+             typename component_size_t = uint16_t>
+    class RawData {
+    public:
+        struct Entity {
+            entity_size_t sz;
+            void*         data;
+        };
+
+        struct Component {
+            component_size_t sz;
+            component_id_t   id;
+        };
+
+        static constexpr component_id_t INVALIDATED_COMPONENT = std::numeric_limits<component_id_t>::max();
+
+        // will always add an entity to the end of the vector
+        size_t AddEntity() {
+            // insert index in _entities
+            _entities.push_back(_ary.size());
+            // insert size in _ary
+            entity_size_t sz = static_cast<entity_size_t>(sizeof(entity_size_t));
+            _ary_append_bytes(&sz, sz, -1);
+            return _entities.size()-1;
+        }
+
+        // will try to reuse an entity
+        size_t AddEntity(size_t expected_size) {
+            // TODO
+            return 0;
+        }
+
+        bool IsEntityValid(size_t entity) {
+            return GetEntitySize(entity) >= 0;
+        }
+
+        entity_size_t GetEntitySize(size_t entity) {
+            return *reinterpret_cast<entity_size_t*>(&_ary[_entities[entity]]);
+        }
+
+        void InvalidateEntity(Entity* entity) {
+            throw; // TODO
+        }
+
+        void RemoveIdentity(Entity* entity) {
+            throw; // TODO
+        }
+
+        void AddComponent(size_t entity, component_size_t sz, component_id_t id, void* data) {
+            // create component
+            Component component { sz, id };
+            // TODO - look for space for a existing component
+            size_t idx = _find_space_for_component(entity, sizeof(Component) + sz);
+            _ary_copy_bytes(&component, sizeof(Component), idx);
+            _ary_copy_bytes(data, sz, idx + sizeof(Component));
+        }
+
+        template<typename F>
+        void ForEachComponent(uint8_t* entity_ptr, F const& f) {
+            entity_size_t entity_sz = *reinterpret_cast<entity_size_t*>(entity_ptr);
+            uint8_t* end = entity_ptr + entity_sz;
+            entity_ptr += sizeof(entity_size_t);
+            while(entity_ptr < end) {
+                Component* component = reinterpret_cast<Component*>(entity_ptr);
+                if(f(component, entity_ptr + sizeof(Component))) {
+                    return;
+                }
+                entity_ptr += component->sz + sizeof(Component);
+            }
+        }
+
+        template<typename F>
+        void InvalidateComponent(size_t entity, component_id_t id, F const& destructor) {
+            uint8_t* entity_ptr = &_ary[_entities[entity]];
+            ForEachComponent(entity_ptr, [&](Component* component, void* data) {
+                if(id == component->id) {
+                    destructor(data);
+                    component->id = INVALIDATED_COMPONENT;
+                    return true;  // stop searching
+                }
+                return false;
+            });
+        }
+
+        void RemoveComponent(size_t entity, component_id_t id) {
+            throw; // TODO
+        }
+
+    private:
+        FRIEND_TEST(RawTest, AddEntity);
+        FRIEND_TEST(RawTest, AddComponents);
+        FRIEND_TEST(RawTest, RemoveComponents);
+
+        std::vector<size_t> _entities = {};
+        std::vector<uint8_t> _ary = {};
+
+        template<typename PTR>
+        void _ary_append_bytes(PTR* origin, size_t sz, ssize_t pos) {
+            if(pos == -1) {
+                pos = _ary.size();
+            }
+            _ary.insert(begin(_ary)+pos, sz, 0);
+            _ary_copy_bytes(origin, sz, pos);
+        }
+
+        template<typename PTR>
+        void _ary_copy_bytes(PTR* origin, size_t sz, size_t pos) {
+            memcpy(&_ary[pos], reinterpret_cast<uint8_t*>(origin), sz);
+        }
+
+        size_t _find_space_for_component(size_t entity, size_t total_sz) {
+            // adjust entity size
+            size_t idx = _entities[entity];
+            entity_size_t entity_sz = *reinterpret_cast<entity_size_t*>(&_ary[idx]);
+            // TODO - reuse inactive components
+            // open space in _ary
+            _ary.insert(begin(_ary) + idx + entity_sz, total_sz, 0);
+            // adjust indexes
+            for(auto it=begin(_entities)+entity+1; it != end(_entities); ++it) {
+                *it += total_sz;
+            }
+            // adjust entity size
+            entity_size_t* sz = reinterpret_cast<entity_size_t*>(&_ary[idx]);
+            *sz = static_cast<entity_size_t>(entity_sz + total_sz);
+            return idx + entity_sz;
+        }
+    };
+
+};
+
+}  // namespace ECS
+
+#endif
+
+// vim: ts=4:sw=4:sts=4:expandtab:foldmethod=marker
--- test/test.cc
+++ test/test.cc
@@ -1,142 +1,9 @@
-#include <cstdint>
-#include <cstdlib>
-#include <cstring>
-#include <limits>
-#include <vector>
-
-#include <gtest/gtest_prod.h>
-
-namespace ECS {
-
-class Engine {
-private:
-    template<typename entity_size_t    = uint32_t,
-             typename component_id_t   = uint16_t,
-             typename component_size_t = uint16_t>
-    class RawData {
-    public:
-        struct Entity {
-            entity_size_t sz;
-            void*         data;
-        };
-
-        struct Component {
-            component_size_t sz;
-            component_id_t   id;
-        };
-
-        static constexpr component_id_t INVALIDATED_COMPONENT = std::numeric_limits<component_id_t>::max();
-
-        // will always add an entity to the end of the vector
-        size_t AddEntity() {
-            // insert index in _entities
-            _entities.push_back(_ary.size());
-            // insert size in _ary
-            entity_size_t sz = static_cast<entity_size_t>(sizeof(entity_size_t));
-            _ary_append_bytes(&sz, sz, -1);
-            return _entities.size()-1;
-        }
-
-        // will try to reuse an entity
-        size_t AddEntity(size_t expected_size) {
-            // TODO
-            return 0;
-        }
-
-        entity_size_t GetEntitySize(size_t entity) {
-            return *reinterpret_cast<entity_size_t*>(&_ary[_entities[entity]]);
-        }
-
-        void InvalidateEntity(Entity* entity) {
-            // TODO
-        }
-
-        void RemoveIdentity(Entity* entity) {
-            // TODO
-        }
-
-        void AddComponent(size_t entity, component_size_t sz, component_id_t id, void* data) {
-            // create component
-            Component component { sz, id };
-            // TODO - look for space for a existing component
-            size_t idx = _find_space_for_component(entity, sizeof(Component) + sz);
-            _ary_copy_bytes(&component, sizeof(Component), idx);
-            _ary_copy_bytes(data, sz, idx + sizeof(Component));
-        }
-
-        template<typename F>
-        void ForEachComponent(uint8_t* entity_ptr, F const& f) {
-            entity_size_t entity_sz = *reinterpret_cast<entity_size_t*>(entity_ptr);
-            uint8_t* end = entity_ptr + entity_sz;
-            entity_ptr += sizeof(entity_size_t);
-            while(entity_ptr < end) {
-                Component* component = reinterpret_cast<Component*>(entity_ptr);
-                if(f(component, entity_ptr + sizeof(Component))) {
-                    return;
-                }
-                entity_ptr += component->sz;
-            }
-        }
-
-        void InvalidateComponent(size_t entity, component_id_t id) {
-            uint8_t* entity_ptr = &_ary[_entities[entity]];
-            ForEachComponent(entity_ptr, [&id](Component* component, void*) {
-                if(id == component->id) {
-                    component->id = INVALIDATED_COMPONENT;
-                    return true;  // stop searching
-                }
-                return false;
-            });
-        }
-
-    private:
-        FRIEND_TEST(RawTest, Basic);
-
-        std::vector<size_t> _entities = {};
-        std::vector<uint8_t> _ary = {};
-
-        template<typename PTR>
-        void _ary_append_bytes(PTR* origin, size_t sz, ssize_t pos) {
-            if(pos == -1) {
-                pos = _ary.size();
-            }
-            _ary.insert(begin(_ary)+pos, sz, 0);
-            _ary_copy_bytes(origin, sz, pos);
-        }
-
-        template<typename PTR>
-        void _ary_copy_bytes(PTR* origin, size_t sz, size_t pos) {
-            memcpy(&_ary[pos], reinterpret_cast<uint8_t*>(origin), sz);
-        }
-
-        size_t _find_space_for_component(size_t entity, size_t total_sz) {
-            // adjust entity size
-            size_t idx = _entities[entity];
-            entity_size_t* entity_sz = reinterpret_cast<entity_size_t*>(&_ary[idx]);
-            // TODO - reuse inactive components
-            // open space in _ary
-            _ary.insert(begin(_ary) + idx + (*entity_sz), total_sz, 0);
-            // adjust indexes
-            for(auto it=begin(_entities)+entity+1; it != end(_entities); ++it) {
-                *it += total_sz;
-            }
-            // adjust entity size
-            (*entity_sz) += total_sz;
-
-            return idx + (*entity_sz);
-        }
-    };
-
-};
-
-}  // namespace ECS
-
-//-----------------------------------------------------
-
 #include <vector>
 using namespace std;
 
 #include <gtest/gtest.h>
+
+#include "fastecs.hh"
 
 namespace ECS {
 
@@ -152,7 +19,7 @@ protected:
     RawTest() : e1(rd.AddEntity()), e2(rd.AddEntity()) {}
 };
 
-TEST_F(RawTest, Basic) {
+TEST_F(RawTest, AddEntity) {
     ASSERT_EQ(e1, 0);
     ASSERT_EQ(rd.GetEntitySize(e1), 4);
 
@@ -161,7 +28,80 @@ TEST_F(RawTest, Basic) {
 
     ASSERT_EQ(rd._entities, vector<size_t>({ 0, 4 }));
     ASSERT_EQ(rd._ary, vector<uint8_t>({ 4, 0, 0, 0, 4, 0, 0, 0 }));
+
+    ASSERT_EQ(rd.IsEntityValid(e1), true);
 }
+
+TEST_F(RawTest, AddComponents) {
+    struct MyComponent {
+        uint16_t a;
+    };
+    MyComponent my = { 42 };
+    rd.AddComponent(e2, sizeof my, 7, &my);
+    ASSERT_EQ(rd._ary, vector<uint8_t>({ 
+        /* entity 0 */           4, 0, 0, 0, 
+        /* entity 1 */           10, 0, 0, 0, 
+        /* component 1:0 size */ 2, 0,
+        /* component 1:0 id */   7, 0,
+        /* component 1:0 data*/  42, 0 }));
+    ASSERT_EQ(rd._entities, vector<size_t>({ 0, 4 }));
+
+    my.a = 33;
+    rd.AddComponent(e1, sizeof my, 5, &my);
+    ASSERT_EQ(rd._ary, vector<uint8_t>({ 
+        /* entity 0 */           10, 0, 0, 0, 
+        /* component 1:0 size */ 2, 0,
+        /* component 1:0 id */   5, 0,
+        /* component 1:0 data*/  33, 0,
+        /* entity 1 */           10, 0, 0, 0, 
+        /* component 1:0 size */ 2, 0,
+        /* component 1:0 id */   7, 0,
+        /* component 1:0 data*/  42, 0 }));
+    ASSERT_EQ(rd._entities, vector<size_t>({ 0, 10 }));
+    
+    struct MySecondComponent {
+        uint8_t b;
+    };
+    MySecondComponent my2 = { 13 };
+    rd.AddComponent(e1, sizeof my2, 2, &my2);
+    ASSERT_EQ(rd._ary, vector<uint8_t>({ 
+        /* entity 0 */           15, 0, 0, 0, 
+        /* component 1:0 size */ 2, 0,
+        /* component 1:0 id */   5, 0,
+        /* component 1:0 data*/  33, 0,
+        /* component 1:1 size */ 1, 0,
+        /* component 1:1 id */   2, 0,
+        /* component 1:1 data*/  13,
+        /* entity 1 */           10, 0, 0, 0, 
+        /* component 1:0 size */ 2, 0,
+        /* component 1:0 id */   7, 0,
+        /* component 1:0 data*/  42, 0 }));
+    ASSERT_EQ(rd._entities, vector<size_t>({ 0, 15 }));
+}
+
+
+bool destroyed = false;
+TEST_F(RawTest, RemoveComponents) {
+    // add two components, to both entites
+    struct MyComponent {
+        uint16_t a;
+        ~MyComponent() { destroyed = true; }
+    };
+    MyComponent my = { 42 };
+    rd.AddComponent(e2, sizeof my, 7, &my);
+
+    auto destructor = [](void*){};
+    
+    rd.InvalidateComponent(e2, 7, destructor);
+    ASSERT_EQ(rd._ary, vector<uint8_t>({ 
+        /* entity 0 */           4, 0, 0, 0, 
+        /* entity 1 */           10, 0, 0, 0, 
+        /* component 1:0 size */ 2, 0,
+        /* component 1:0 id */   0xFF, 0xFF,
+        /* component 1:0 data*/  42, 0 }));
+    ASSERT_EQ(destroyed, true);
+}
+
 
 }
 
@@ -169,67 +109,5 @@ int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
-#if 0
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
-
-using namespace std;
-
-TEST_CASE("Add raw entities", "[entities]") {
-
-    using RawData = ECS::Engine::RawData<>;
-    RawData rd;
-    size_t e = rd.AddEntity();
-    REQUIRE(e == 0);
-
-    REQUIRE(rd.GetEntitySize(e) == 4);
-
-    e = rd.AddEntity();
-    REQUIRE(e == 1);
-
-    REQUIRE(rd.GetEntitySize(e) == 4);
-
-    REQUIRE(rd._entities == vector<size_t>({ 0, 4 }));
-    REQUIRE(rd._ary == vector<uint8_t>({ 4, 0, 0, 0, 4, 0, 0, 0 }));
-
-    struct MyComponent {
-        uint16_t a;
-    };
-    MyComponent my = { 42 };
-    rd.AddComponent(e, sizeof my, 7, &my);
-    REQUIRE(rd._ary == vector<uint8_t>({ 
-        /* entity 0 */ 4, 0, 0, 0, 
-        /* entity 1 */ 10, 0, 0, 0, 
-        /* component 1:0 size */ 2, 0,
-        /* component 1:0 id */   7, 0,
-        /* component 1:0 data*/  42, 0 }));
-    REQUIRE(rd._entities == vector<size_t>({ 0, 4 }));
-
-    my.a = 33;
-    rd.AddComponent(0, sizeof my, 5, &my);
-    REQUIRE(rd._ary == vector<uint8_t>({ 
-        /* entity 0 */ 10, 0, 0, 0, 
-        /* component 1:0 size */ 2, 0,
-        /* component 1:0 id */   5, 0,
-        /* component 1:0 data*/  33, 0,
-        /* entity 1 */ 10, 0, 0, 0, 
-        /* component 1:0 size */ 2, 0,
-        /* component 1:0 id */   7, 0,
-        /* component 1:0 data*/  42, 0 }));
-    REQUIRE(rd._entities == vector<size_t>({ 0, 10 }));
-
-    rd.InvalidateComponent(e, sizeof(MyComponent));
-    REQUIRE(rd._ary == vector<uint8_t>({ 
-        /* entity 0 */ 10, 0, 0, 0, 
-        /* component 1:0 size */ 2, 0,
-        /* component 1:0 id */   5, 0,
-        /* component 1:0 data*/  33, 0,
-        /* entity 1 */ 10, 0, 0, 0, 
-        /* component 1:0 size */ 2, 0,
-        /* component 1:0 id */   0xFF, 0xFF,
-        /* component 1:0 data*/  42, 0 }));
-}
-#endif
 
 // vim: ts=4:sw=4:sts=4:expandtab:foldmethod=marker
