4969ebbcfb0670f54b0f112c1b46fb1873205ed6
André Wagner <andre.nho@gmail.com>
2016-05-23 22:10:14 -0300

    .

--- test/test.cc
+++ test/test.cc
@@ -19,10 +19,20 @@ private:
             void*         data;
         };
 
-        size_t AddEntity(Entity** entity) {
+        // will always add an entity to the end of the vector
+        size_t AddEntity() {
+            _entities.push_back(_ary.size());
+            entity_size_t sz = static_cast<entity_size_t>(sizeof(entity_size_t));
+            std::copy(&sz, &sz + sizeof(entity_size_t), std::back_inserter(_ary));
+            return _entities.size()-1;
         }
 
-        Entity* GetEntity(size_t id) {
+        // will try to reuse an entity
+        size_t AddEntity(size_t expected_size) {
+        }
+
+        Entity* GetEntity(size_t entity) {
+            return reinterpret_cast<Entity*>(&_ary[entity]);
         }
 
         void InvalidateEntity(Entity* entity) {
@@ -33,7 +43,7 @@ private:
 
     private:
         std::vector<size_t> _entities = {};
-        std::vector<uint8_t> _mega_array = {};
+        std::vector<uint8_t> _ary = {};
     };
 
 };
@@ -51,10 +61,11 @@ TEST_CASE("Manage raw entities", "[entities]") {
 
     using RawData = ECS::Engine::RawData<>;
     RawData rd;
-    RawData::Entity* entity;
-    size_t n = rd.AddEntity(&entity);
-    REQUIRE(n == 0);
+    size_t e = rd.AddEntity();
+    REQUIRE(e == 0);
 
+    RawData::Entity* ent = rd.GetEntity(e);
+    REQUIRE(ent->sz == 4);
 }
 
 // vim: ts=4:sw=4:sts=4:expandtab:foldmethod=marker
